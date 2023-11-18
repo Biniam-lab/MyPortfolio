52e321371fb2b8e2e77604e68a58a56f2cafbe22
Biniam <109479760+Biniam-lab@users.noreply.github.com>
2023-11-18 14:45:57 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,78 +1,15 @@
-/* document.addEventListener("DOMContentLoaded", () => {
-    const links = document.querySelectorAll(".links");
-    const containerContents = document.querySelectorAll(".main-container-content");
-    const toggleButton = document.getElementsByClassName('toggle-button')[0];
-    const navbarLinks = document.getElementsByClassName('navbar-links')[0];
-
-    toggleButton.addEventListener('click', () => {
-        navbarLinks.classList.toggle('active')
-    })
-
-    document.getElementById("about-me-section").style.display = "block";
-
-    links.forEach((link) => {
-        link.addEventListener("click", (e) => {
-            e.preventDefault();
-            const dataTarget = link.getAttribute("data-target");
-            containerContents.forEach((containerContent) => {
-                containerContent.style.display = "none";
-                link.classList.toggle("active");
-
-            });
-
-            document.getElementById(dataTarget).style.display = "block";
-            link.classList.toggle("active");
-
-        });
-    });
-
-}); 
-const selectedNavLink = navLink.getAttribute('data-target');
-        const mainContainerContents = document.querySelectorAll('.main-container-content');
-        mainContainerContents.forEach((mainContainerContent) => {
-            mainContainerContent.style.display = 'none';
-        });
-        document.getElementById("about-me-section").style.display = "block";
-        
-    document.getElementById("about-me-section").style.display = "block";
-*/
-
-/* const mainContainer = document.querySelectorAll(".main-container-content");
-switch (id) {
-    case "about-me-section":
-        console.log(id);
-        mainContainer.style.display = "none";
-        document.getElementById("about-me-section").style.display = "block";
-        break;
-    case "education-and-certification-section":
-        console.log(id);
-        break;
-    case "projects-and-experience-section":
-        console.log(id);
-        break;
-    case "contact-info-section":
-        console.log(id);
-        break;
-
-} */
-const mainContainerContents = document.querySelectorAll('.main-container-content');
+/* const mainContainerContents = document.querySelectorAll('.main-container-content');
 const navLinks = document.querySelectorAll(".nav-link");
-const button = document.getElementById("navbarTogglerButton");
+const menuButton = document.getElementById('navbarTogglerButton');
 
 document.addEventListener("DOMContentLoaded", () => {
-
-    mainContainerContents.forEach((mainContainerContent) => {
-        if (mainContainerContent.id == 'about-me-section')
-            mainContainerContent.style.display = 'block';
-        else
-            mainContainerContent.style.display = 'none';
-    });
 
     navLinks.forEach((navLink) => {
         navLink.addEventListener('click', (event) => {
             console.log(event);
-            showHideContent(event);
+            //showHideContent(event);
             activeNavLink(event);
+            menuButton.click();
         });
     });
 
@@ -83,7 +20,7 @@ function showHideContent(event) {
     mainContainerContents.forEach((mainContainerContent) => {
         if (mainContainerContent.id == event.target.dataset.target) {
             mainContainerContent.style.display = 'flex';
-            button.click();
+
         }
         else {
             mainContainerContent.style.display = 'none';
@@ -92,7 +29,6 @@ function showHideContent(event) {
 }
 
 function activeNavLink(event) {
-
     navLinks.forEach((navLink) => {
         if (navLink.getAttribute('data-target') == event.target.dataset.target) {
             navLink.classList.add('active');
@@ -101,4 +37,4 @@ function activeNavLink(event) {
             navLink.classList.remove('active');
         }
     });
-}
+} */
